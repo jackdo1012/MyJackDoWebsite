452b833c7f73b568f896c1f7aa02f9eb476e06ae
Jack Do <dominhtrijack@gmail.com>
2020-09-27 11:33:52 +0700

    27/9/2020

--- js/update-cart.js
+++ js/update-cart.js
@@ -19,10 +19,6 @@ function setFirstNum() {
     num8 = 0;
 }
 
-if (num1, num2, num3, num4, num5, num6, num7, num8 !== Number) {
-    setFirstNum();
-}
-
 function updateNumInCart() {
     totalNum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8;
     document.getElementById("num-of-item").innerHTML = '( Total: ' + totalNum + ' )';
